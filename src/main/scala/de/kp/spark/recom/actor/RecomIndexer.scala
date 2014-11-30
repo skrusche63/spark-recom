bc0c2d364a5766d84150a0abc96b81d5e7fd8bcf
skrusche63 <skrusche63@googlemail.com>
2014-11-30 14:27:20 +0100

    ElasticBuilderFactory moved to 'spark-core'.

--- src/main/scala/de/kp/spark/recom/actor/RecomIndexer.scala
+++ src/main/scala/de/kp/spark/recom/actor/RecomIndexer.scala
@@ -19,10 +19,11 @@ package de.kp.spark.recom.actor
  */
 
 import de.kp.spark.core.model._
+import de.kp.spark.core.elastic.{ElasticBuilderFactory => EBF}
+
 import de.kp.spark.core.io.ElasticIndexer
 
 import de.kp.spark.recom.model._
-import de.kp.spark.recom.io.{ElasticBuilderFactory => EBF}
 
 class RecomIndexer extends BaseActor {
   
